c88ceacda9f63dd978a3b6be6f41ee94822c119b
mustafa muse <muse3mm@gmail.com>
2022-06-02 21:39:46 +0200

    change css import.

--- app/src/app/App.vue
+++ app/src/app/App.vue
@@ -1,21 +1,15 @@
 <template>
   <Header />
   <RouterView />
-  
-  
-  
 </template>
 
 <script>
-import Header from "../components/Header.vue";
-//import Footer from "../components/Footer.vue";
+  import Header from "../components/Header.vue";
+  //import Footer from "../components/Footer.vue";
 
   export default {
     components: {
       Header,
-       
-      
-     
     },
   };
 </script>
